31f2211fd79f2ff178cc24e1a76adaf2c455cec5
Zhukov <aleksandr.zhukov@sevenonemedia.de>
2018-10-02 17:32:12 +0200

    handle js

--- packer.py
+++ packer.py
@@ -5,6 +5,7 @@ import fnmatch
 import os
 import json
 import tempfile
+import subprocess
 
 
 
@@ -14,13 +15,7 @@ except NameError:
     pass
 
 
-# run shell commands
-# import subprocess
 
-# p = subprocess.Popen('ls -la', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='/var/folders/jg/c8j3dyfx3h3bfnk_z538x24m5xj24d/T/1iLhKb')
-# for line in p.stdout.readlines():
-    # print line,
-# retval = p.wait()
 
 
 #read node config
@@ -38,26 +33,34 @@ this_dir = os.path.join(cwd, '')
 def cast_list(val):
     return [val] if type(val) is not list else val
 
+def exec_command(command, cwd = None, silent = True):
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd)
+    return p.communicate()
 
-#config functions
 
 def load_json(file_name):
     with open(file_name) as json_file:
         return json.load(json_file)
 
+def load_js(file_name):
+    res = exec_command('node -p "JSON.stringify(require(\'./{}\'))"'.format(file_name))
+    return json.loads(res[0])
+
+# print('---')
+# print(load_js('packer.config.js'))
+# print(load_json('packer.config.json'))
+
+# exit()
+
 config_paths = [
-  # {
-    # 'fileName': 'packer.config.js',
-    # 'load': load_json
-  # },
   {
     'fileName': 'packer.config.json',
     'load': load_json
   },
-  # {
-    # 'fileName': 'packer.config.yml',
-    # # load: (fileName) => {/* load yml */}
-  # }
+  {
+    'fileName': 'packer.config.js',
+    'load': load_js
+  },
 ]
 
 def load_config():
