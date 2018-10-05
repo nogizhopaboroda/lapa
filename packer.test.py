d9decfe78f8aa2f5ad8f93eb8a1055d2a583fc74
Zhukov <aleksandr.zhukov@sevenonemedia.de>
2018-10-05 16:30:48 +0200

    test install dependencies

--- packer.test.py
+++ packer.test.py
@@ -1,16 +1,25 @@
 import unittest
 import os
 
-from packer import DEFAULT_CONFIG, load_config, enhance_config
+from unittest.mock import patch
+
+from packer import DEFAULT_CONFIG, load_config, enhance_config, exec_command, install_dependencies
 import packer
 
 cwd = os.getcwd()
 
 
+# handle config
+
 class LoadConfig(unittest.TestCase):
     def test_example_1(self):
         config = load_config(os.path.join(cwd, 'example/python/simple'))
         self.assertNotEqual(config, {})
+
+    def test_example_2(self):
+        config = load_config(os.path.join(cwd, 'example/node/lambda_plus_edge'))
+        self.assertNotEqual(config, {})
+        self.assertEqual(len(config), 2)
 
 class EnhanceConfig(unittest.TestCase):
     def test_casts_list_of_configs(self):
@@ -34,6 +43,17 @@ class EnhanceConfig(unittest.TestCase):
         self.assertEqual(config['files'], ['*'])
         self.assertEqual(config['ignore'], ['*'])
         self.assertEqual(config['dependencies'], ['all'])
+
+
+# environments
+@patch('packer.exec_command')
+class InstallDependencies(unittest.TestCase):
+
+    def test_case_1(self, test_patch):
+        test_patch.return_value = 'ok'
+        ret = install_dependencies({"environment": "node", "dependencies": ["foo", "baz"], "tempDir": "/bar"})
+        test_patch.assert_called_with('npm install foo baz', cwd = '/bar')
+
 
 if __name__ == '__main__':
     unittest.main()
